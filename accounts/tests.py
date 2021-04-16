7b0c06dce1c0b5bee069e4aae2bbd5e79883ca36
Jason Painter <jap1@umbc.edu>
2021-04-15 22:19:26 -0400

    Added tests and comments

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.test.client import Client, RequestFactory
+from django.test.utils import override_settings
+from allauth.tests import Mock, TestCase, patch
+from allauth.utils import get_user_model, get_username_max_length
 
-# Create your tests here.
+
+class TestLogin(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.username = "test@test.com"
+        self.password = "password"
+        self.user = get_user_model().objects.create_user(username=self.username, password=self.password)
+
+    def test_login_page(self):
+        response = self.client.get(reverse("account_login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        response = self.client.post(reverse(
+            'account_login'), {"login": self.username, "password": self.password}
+        )
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL, fetch_redirect_response=False)
+
+
--- dataPrivacy/settings.py
+++ dataPrivacy/settings.py
@@ -39,18 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # Add more providers here ex: 'allauth.socialaccount.providers.amazon'
     'allauth.socialaccount.providers.google',
 
     'page',
+    'accounts',
 ]
 
 SITE_ID = 1
-
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
@@ -86,12 +87,21 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+
+                # `allauth` needs this from django
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 WSGI_APPLICATION = 'dataPrivacy.wsgi.application'
 
 
