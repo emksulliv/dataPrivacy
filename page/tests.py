7f128eae2f9ee7627c194d18b51c9eda3ad78738
pauldwrona <pauldwrona@gmail.com>
2021-04-15 23:32:01 -0400

    Models and tests for models added

--- page/tests.py
+++ page/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.shortcuts import render
 from page.views import index
+from page.models import User
 
 # Create your tests here.
 
@@ -20,3 +22,22 @@ class HomePageTest(TestCase):
         self.assertIn(
             '<div id="myModal" class="modal fade" role="dialog">', html)
         self.assertTrue(html.endswith('</html>'))
+
+
+class LoginTest(TestCase):
+    def test_check_login(self):
+        request = HttpRequest()
+        response = render(request, 'login.html')
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>Login</title>', html)
+
+class DBTest(TestCase):
+    def setUp(self):
+        User.objects.create(session_id="1", user_name="Bob", user_token="123")
+        User.objects.create(session_id="2", user_name="Jane", user_token="456")
+
+    def test_properly_added(self):
+        bob = User.objects.get(user_name="Bob")
+        jane = User.objects.get(user_name="Jane")
+        self.assertTrue(bob)
