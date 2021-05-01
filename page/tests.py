78e33ce76f9cc686ce730cbb64ccefee6e1ccfde
Paul Wrona <pauldwrona@gmail.com>
2021-04-30 23:02:01 -0400

    Added tests and facebook redirect

--- page/tests.py
+++ page/tests.py
@@ -1,7 +1,21 @@
+from allauth.account import app_settings as account_settings
+from allauth.account.models import EmailAddress
+from allauth.account.utils import user_email
+from allauth.socialaccount.helpers import complete_social_login
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialLogin
+from allauth.utils import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import User
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import TestCase
+from django.test.client import Client
+from django.test.client import RequestFactory
+from django.test.utils import override_settings
 from django.urls import resolve
 from django.http import HttpRequest
 from django.shortcuts import render
+
 from page.views import index
 from page.models import User
 import requests
@@ -63,3 +77,38 @@ class DBTest(TestCase):
         bob = User.objects.get(user_name="Bob")
         jane = User.objects.get(user_name="Jane")
         self.assertTrue(bob)
+
+
+class SocialAccountTests(TestCase):
+
+    @override_settings(
+        SOCIALACCOUNT_AUTO_SIGNUP=True,
+        ACCOUNT_SIGNUP_FORM_CLASS=None,
+        ACCOUNT_EMAIL_VERIFICATION=account_settings.EmailVerificationMethod.NONE  # noqa
+    )
+    def test_email_address_created(self):
+        factory = RequestFactory()
+        request = factory.get('/accounts/login/callback/')
+        request.user = AnonymousUser()
+        SessionMiddleware().process_request(request)
+        MessageMiddleware().process_request(request)
+
+        User = get_user_model()
+        user = User()
+        setattr(user, account_settings.USER_MODEL_USERNAME_FIELD, 'test')
+        setattr(user, account_settings.USER_MODEL_EMAIL_FIELD, 'test@test.com')
+
+        account = SocialAccount(user=user, provider='openid', uid='123')
+        sociallogin = SocialLogin(account)
+        complete_social_login(request, sociallogin)
+
+        user = User.objects.get(
+        **{account_settings.USER_MODEL_USERNAME_FIELD: 'test'}
+        )
+        self.assertTrue(
+            SocialAccount.objects.filter(user=user, uid=account.uid).exists()
+        )
+        self.assertTrue(
+            EmailAddress.objects.filter(user=user,
+                                        email=user_email(user)).exists()
+        )
